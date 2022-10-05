78b8d871b0a4e9b8700b337384de48278fea6e2f
NikitaV <nikita.volkov0505@mail.ru>
2022-10-05 19:00:49 +0300

    Added models Ingredient, Taco and TacoOrder

---
+++ src/main/java/raven/hub/tacocloud/models/Taco.java
@@ -0,0 +1,11 @@
+package raven.hub.tacocloud.models;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+    private String name;
+    private List<Ingredient> ingredients;
+}
