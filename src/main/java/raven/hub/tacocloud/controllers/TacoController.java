d5ff65c76b3a01ab4a26d8c38a9b5ca738a875b7
NikitaV <nikita.volkov0505@mail.ru>
2022-10-05 18:29:12 +0300

    Fisrt controller and test

--- src/main/java/raven/hub/tacocloud/controllers/TacoController.java
+++ src/main/java/raven/hub/tacocloud/controllers/TacoController.java
@@ -2,9 +2,12 @@ package raven.hub.tacocloud.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
+@RequestMapping("/taco")
 public class TacoController {
-    @GetMapping("/")
+    @GetMapping("/home")
     public String home() {
         return "home";
     }
---
+++ src/test/java/raven/hub/tacocloud/TacoControllerTest.java
@@ -0,0 +1,27 @@
+package raven.hub.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import raven.hub.tacocloud.controllers.TacoController;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@WebMvcTest(TacoController.class)
+public class TacoControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/home"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(
+                        containsString("Welcome to...")));
+    }
+}
